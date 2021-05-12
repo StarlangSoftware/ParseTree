1f3856b5192d1aa3b3f821e910d95b1a8744ad2b
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-05-12 13:36:29 +0300

    In NodeCollector if the condition is null, NodeCollector collects all nodes in the tree.

--- src/main/java/ParseTree/NodeCollector.java
+++ src/main/java/ParseTree/NodeCollector.java
@@ -26,7 +26,7 @@ public class NodeCollector {
      * @param collected The {@link ArrayList} where the collected ParseNode's will be stored.
      */
     private void collectNodes(ParseNode parseNode, ArrayList<ParseNode> collected){
-        if (condition.satisfies(parseNode)){
+        if (condition == null || condition.satisfies(parseNode)){
             collected.add(parseNode);
         } else {
             for (int i = 0; i < parseNode.numberOfChildren(); i++){
